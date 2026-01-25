887241ddbed25bb0614ff42335a277437812390f
mingli202 <vincentmingli@gmail.com>
2026-01-24 19:50:20 -0500

    updated

--- web/convex/_generated/api.d.ts
+++ web/convex/_generated/api.d.ts
@@ -8,13 +8,35 @@
  * @module
  */
 
+import type * as actions_fetchShorts from "../actions/fetchShorts.js";
+import type * as actions_generateOutline from "../actions/generateOutline.js";
+import type * as mutations_appendFeedItems from "../mutations/appendFeedItems.js";
+import type * as mutations_appendOutlineItems from "../mutations/appendOutlineItems.js";
+import type * as mutations_createPrompt from "../mutations/createPrompt.js";
+import type * as mutations_deletePrompt from "../mutations/deletePrompt.js";
+import type * as mutations_updatePromptProgress from "../mutations/updatePromptProgress.js";
+import type * as queries_getPrompt from "../queries/getPrompt.js";
+import type * as queries_listFeedItems from "../queries/listFeedItems.js";
+import type * as queries_listPrompts from "../queries/listPrompts.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
 
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  "actions/fetchShorts": typeof actions_fetchShorts;
+  "actions/generateOutline": typeof actions_generateOutline;
+  "mutations/appendFeedItems": typeof mutations_appendFeedItems;
+  "mutations/appendOutlineItems": typeof mutations_appendOutlineItems;
+  "mutations/createPrompt": typeof mutations_createPrompt;
+  "mutations/deletePrompt": typeof mutations_deletePrompt;
+  "mutations/updatePromptProgress": typeof mutations_updatePromptProgress;
+  "queries/getPrompt": typeof queries_getPrompt;
+  "queries/listFeedItems": typeof queries_listFeedItems;
+  "queries/listPrompts": typeof queries_listPrompts;
+}>;
 
 /**
  * A utility for referencing Convex functions in your app's public API.
